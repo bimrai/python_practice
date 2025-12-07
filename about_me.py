90460c0bbf7f536d6a605e7f85fcc949b6afdaa2
Bim Rai <166420374+bimrai@users.noreply.github.com>
2025-12-07 03:12:30 +0000

    Add personal information and transfer details

---
+++ about_me.py
@@ -0,0 +1,9 @@
+name = "Bim Rai"
+age = 24 # the time of coding
+subject = "BSc Computer Science"
+university = "University of London"
+university_1 = "Cardiff University"
+university_2 = "University of Reading"
+
+print(f'The student named {name} is aged {age}, currently studying {subject}, at the {university}.')
+print(f'{name} is looking to transfer University. The offers received so far are from {university_1} and the {university_2}. {name} is likely to choose {university_1} over {university_2} due to 2nd Year entry point.')
