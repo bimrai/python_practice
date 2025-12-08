e0ae127b4a37b68b742c289b399f6da3ccd585d8
Bim Rai <166420374+bimrai@users.noreply.github.com>
2025-12-08 04:54:44 +0000

    Add files via upload

---
+++ small_practice/multiline_string.py
@@ -0,0 +1,11 @@
+multiline_string = """Hello
+World!"""
+
+multiline_string_2 = '''Goodbye
+World!
+Or rather,
+I'll see you soon!'''
+
+print(multiline_string)
+print('===========')
+print(multiline_string_2)
