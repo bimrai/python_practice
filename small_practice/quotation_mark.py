00ac06bf52917199e6a29bac172309ee19025221
Bim Rai <166420374+bimrai@users.noreply.github.com>
2025-12-08 05:06:01 +0000

    Enhance string handling in quotation_mark.py
    Added comments and improved string formatting.

---
+++ small_practice/quotation_mark.py
@@ -0,0 +1,8 @@
+# practice working with quotation marks within string
+spiderman_intro = 'I\'m Spider-Man.'
+spiderman_quote = "Uncle Ben told me, \"With great power, comes great responsibility\"."
+mary_jane_intro = "I'm Mary Jane Watson."
+
+print(f'Spider-Man: {spiderman_intro}')
+print(f'Peter Parker: {spiderman_quote}')
+print(f'Mary Jane Watson: {mary_jane_intro}')
