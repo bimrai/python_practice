08852ae7e4352e47d6bda7c7fa066d73e3fbde87
Bim Rai <166420374+bimrai@users.noreply.github.com>
2025-12-24 00:08:23 +0000

    Refactor love calculator logic and random score generation

--- love_calculator.py
+++ love_calculator.py
@@ -1,17 +1,21 @@
+import random
+
 name1 = input('Your name: ')
 name2 = input('Their name: ')
 
-gen_num = range(0, 101)
+gen_num = random.randint(0, 100)
+
+print(f"\nMatch score: {gen_num}%")
 
 if gen_num == 100:
     print(f'You and {name2} are a perfect match!')
-elif gen_num > 80 and gen_num < 91:
+elif 80 < gen_num <= 90:
     print('Great match!')
-elif gen_num > 70 and gen_num < 81:
+elif 70 < gen_num <= 80:
     print(f'You and {name2} have potential')
-elif gen_num > 50 and gen_num < 71:
+elif 50 < gen_num <= 70:
     print('Steps ahead look promising!')
-elif gen_num > 20 and gen_num < 51:
+elif 20 < gen_num <= 50:
     print('Not looking great...')
-elif gen_num > 0 and gen_num < 21:
+else:
     print('Weak match')
