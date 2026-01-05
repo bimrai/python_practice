4a62665036a98ac4a43d4958adeee483cb686630
Bim Rai <166420374+bimrai@users.noreply.github.com>
2026-01-05 23:58:07 +0000

    Update number_doubler.py

--- number_doubler.py
+++ number_doubler.py
@@ -1,8 +1,11 @@
+# a number doubler
+
 print("Number Doubler")
 
 def numDoubler():
     num = int(input('Enter Number: '))
     double = num*2
     print(f'Number: {num}, Number Doubled: {double}!')
+
 
 numDoubler()
