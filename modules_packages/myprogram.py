0e051345eeef27e153f57af75739e216ef4cd29b
Bim Rai <166420374+bimrai@users.noreply.github.com>
2026-02-03 00:36:46 +0000

    Working with modules and packages

---
+++ modules_packages/myprogram.py
@@ -0,0 +1,16 @@
+# from mymodule import my_func
+
+# my_func()
+
+
+
+
+# import from package:
+
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import mysubscript
+
+# call from some_main_script
+some_main_script.report_main()
+
+mysubscript.sub_report()
