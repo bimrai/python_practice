00dedb2e20257cb660882178a42511950de1658f
Bim Rai <166420374+bimrai@users.noreply.github.com>
2026-02-06 22:44:44 +0000

    pylint testing

---
+++ errors_exceptions_handling/simple1.py
@@ -0,0 +1,20 @@
+'''
+simple script
+'''
+
+def myfunc():
+    '''
+    simple func
+    '''
+     
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
+
+'''
+Using pylint via terminal 'pylint simpl1.py', we can check errors and score of
+written code. Usually used in big projects. Can be useful then, or solo. 
+'''
