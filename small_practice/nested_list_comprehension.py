8130b72acc5532140817371fa87d8458c2d1ce52
Bim Rai <166420374+bimrai@users.noreply.github.com>
2026-02-26 18:53:54 +0000

    Add nested list comprehension to filter names

---
+++ small_practice/nested_list_comprehension.py
@@ -0,0 +1,9 @@
+all_data = [["John", "Emily", "Michael", "Mary", "Steven"], ["Maria", "Juan","Javier", "Natalia", "Pilar"] ]
+
+name_with_two_a = []
+
+for names in all_data:
+    more_a = [name for name in names if name.count('a') >= 2]
+    name_with_two_a.extend(more_a)
+    
+print(name_with_two_a)
